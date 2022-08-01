da2b0c3b8ac3d5607de6164d5d70cb066d823c1c
AJ McCrory <93270610+Ajmccrory@users.noreply.github.com>
2022-08-01 19:39:56 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,40 @@
+import os
+import sys
+
+from setuptools import setup
+
+if sys.version_info < (2, 7) or sys.version_info < (3, 6):
+    sys.exit('Sorry, Python < 2.7 is not supported. Please use Python 3.7+.')
+
+MAJOR_VERSION = 0
+MINOR_VERSION = 0
+REVISION = 2
+
+PACKAGE_VERSION = '.'.join([
+    str(item) for item in [MAJOR_VERSION, MINOR_VERSION, REVISION]
+])
+
+DEPENDENCIES = [
+    'flake8',
+    'praw',
+    'progressbar'
+]
+
+setup(
+    name='reddit_voter',
+    version=PACKAGE_VERSION,
+    description=DESCRIPTION,
+    long_description=README,
+    keywords='reddit',
+    author='AJ McCrory',
+    author_email='ajmccrory10@outlook.com',
+    license='',
+    packages=['reddit_voter'],
+    include_package_data=True,
+    install_requires=DEPENDENCIES,
+    entry_points="""
+    # -*- Entry points: -*-
+    [console_scripts]
+    voter = reddit_voter.reddit_voter:main
+    """,
+)
